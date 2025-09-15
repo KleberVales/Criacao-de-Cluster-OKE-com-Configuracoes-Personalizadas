02140319cf3c01f7cfcd52858547d277f678f8e9
Kleber Vales <klebervales.dev@gmail.com>
2025-09-15 16:32:22 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,22 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {
+  default = "sa-saopaulo-1"
+}
+
+variable "compartment_ocid" {}
+variable "cluster_name" {
+  default = "vision-cluster"
+}
+variable "kubernetes_version" {
+  default = "v1.28.2"
+}
+variable "node_shape" {
+  default = "VM.Standard3.Flex"
+}
+variable "node_count" {
+  default = 3
+}
 
